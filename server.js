e5eb5cfd691f8421630f149f71210fd9ce5e7228
joltdev <joltdev@Jolts-MacBook-Pro.local>
2016-12-08 11:37:46 -0700

    favico

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
-app.enable('trust proxy');
-
-const ensureSecure = (req, res, next) => {
-  if(req.secure){
-    return next();
-  };
-  res.redirect('https://' + req.hostname + req.url);
-};
 
 app.get('*', (req, res)=> {
 	res.sendFile(path.resolve(__dirname, 'index.html'));
