c8ceeea82ba3b4b7515551db6ad09f45e4d7cb49
joltdev <joltdev@Jolts-MacBook-Pro.local>
2016-12-08 10:02:33 -0700

    force ssl

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+var forceSSL = require('express-force-ssl');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
+app.use(forceSSL);
 
 app.get('*', (req, res)=> {
 	res.sendFile(path.resolve(__dirname, 'index.html'));
